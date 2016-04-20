52d55b86e5e6af44f1eef6a79309a540cfe4c049
trevion Martin <trevionm17@gmail.com>
2016-04-20 15:25:09 -0400

    good

--- examples.py
+++ examples.py
@@ -1,8 +1,8 @@
 def main():
     choice = input('Press q to Quit or E to Enter:')
 
-    while choice == "E":
-        deg_fahrenheit = float(input("How many degrees Fahrenheit is it outside?"))
+    while choice == "E"or choice=="e":
+        deg_fahrenheit = float(input("Put fahrenheit to get degrees in celsius:"))
         deg_celsius = int((deg_fahrenheit - 32) / 1.8)
         print (deg_fahrenheit,"fahrenheit converts to", deg_celsius, "degrees Celsius .")
         choice = input('Press q to Quit or E to Enter:')
