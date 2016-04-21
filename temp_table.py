3d5988cb9f336c3189d5eff45cc7f12573fd6b40
trev3rd <trevionm17@gmail.com>
2016-04-21 10:16:03 -0400

    Added files via upload

--- temp_table.py
+++ temp_table.py
@@ -1,7 +1,7 @@
 def main():
 	for i in range(0,21):
 		celsius = i * 5
-		fehrenheit = ((9.0/5.0)*(celsius)) + 32
+		fehrenheit = ((1.8)*(celsius)) + 32
 		print (celsius,"C is equal to", fehrenheit,"F")
 main()
 
