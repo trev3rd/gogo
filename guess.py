3b757ab07d64dd9ee937b672b9ca466474e7c450
trevion Martin <trevionm17@gmail.com>
2016-04-26 12:08:22 -0400

    classwork

--- guess.py
+++ guess.py
@@ -4,6 +4,7 @@ import random
 guessesTaken = 0
 
 number = random.randint(1, 10)
+print(number)
 
 
 print(' I am thinking of a number between 1 and 10.')
@@ -27,7 +28,8 @@ while guessesTaken < 3:
 
         print('Your guess is warm.')
 
-    if guess == number +3 or guess == number -3:
+    elif guess > number +2 or guess < number -3:
+
         print('your guess is cold')
 
     if guess == number:
